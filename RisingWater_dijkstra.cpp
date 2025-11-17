204ad9f87245e73a9349a05cea3acd0ae6efe022
liuchengLYC <liucheng05010501@gmail.com>
2025-11-17 20:40:45 +0800

    778

---
+++ RisingWater_dijkstra.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int swimInWater(vector<vector<int>> &grid) {
+        int m = grid.size(), n = grid[0].size();
+        using pii = pair<int, int>;
+        using ipii = pair<int, pii>;
+        int dx[] = {1, -1, 0, 0}, dy[] = {0, 0, 1, -1};
+        priority_queue<ipii, vector<ipii>, greater<>> pq;
+        vector<bool> visited(m * n, 0);
+        pq.push({grid[0][0], {0, 0}}), visited[0] = 1;
+        auto isValid = [&](int r, int c) -> bool {
+            return r >= 0 && c >= 0 && r < m && c < n;
+        };
+        while (!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            int t = top.first, r = top.second.first, c = top.second.second;
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dx[i], nc = c + dy[i];
+                if (isValid(nr, nc) && !visited[nr * n + nc]) {
+                    if (nr == m - 1 && nc == n - 1)
+                        return max(grid[nr][nc], t);
+                    pq.push({max(grid[nr][nc], t), {nr, nc}});
+                    visited[nr * n + nc] = 1;
+                }
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+};
+int main() {
+    Solution s;
+    vector<vector<int>> grid = {{0, 1, 2}, {5, 100, 3}, {6, 7, 8}};
+    cout << s.swimInWater(grid) << endl;
+}
---
+++ RisingWater_dsu.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+class DSU {
+  private:
+    vector<int> parent, rank;
+
+  public:
+    DSU(int n) {
+        parent.resize(n);
+        rank.assign(n, 0);
+        iota(parent.begin(), parent.end(), 0);
+    }
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    void unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b)
+            return;
+        if (rank[a] < rank[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+};
+
+class Solution {
+  public:
+    int swimInWater(vector<vector<int>> &grid) {
+        int n = grid.size();
+        vector<array<int, 3>> edges;
+        auto id = [&](int x, int y) { return x * n + y; };
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i + 1 < n) {
+                    int w = max(grid[i][j], grid[i + 1][j]);
+                    edges.push_back({w, id(i, j), id(i + 1, j)});
+                }
+                if (j + 1 < n) {
+                    int w = max(grid[i][j], grid[i][j + 1]);
+                    edges.push_back({w, id(i, j), id(i, j + 1)});
+                }
+            }
+        }
+        sort(edges.begin(), edges.end());
+        DSU dsu(n * n);
+        for (auto &e : edges) {
+            int w = e[0], u = e[1], v = e[2];
+            dsu.unite(u, v);
+            if (dsu.find(0) == dsu.find(n * n - 1)) {
+                return w;
+            }
+        }
+        return 0;
+    }
+};
